78b3e6437d5aaad2fb8fe098668da9ad79d8bd0a
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-12-29 20:28:45 -0500

    Try another way for linking stdc++ in msys

--- build.rs
+++ build.rs
@@ -32,7 +32,9 @@ fn main() {
     println!("cargo:rustc-link-lib=static=assimp");
 
     // Link to libstdc++
-    if target.contains("windows-gnu") || !target.contains("windows") {
+    if target.contains("windows-gnu") {
+        println!("cargo:rustc-flags=-l dylib=stdc++");
+    } else if !target.contains("windows") {
         println!("cargo:rustc-link-lib=stdc++");
     }
 
