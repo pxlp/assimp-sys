20c73314b892a31fd931a78745374b4f86057969
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-10 18:41:52 -0400

    Add lib link to Cargo.toml

--- build.rs
+++ build.rs
@@ -7,9 +7,8 @@ fn main() {
     let assimp_path = format!("{}/../pixelport_deps/assimp/v3.2/{}/", module_path, target);
     println!("cargo:rustc-link-search=native={}", assimp_path);
 
-    println!("cargo:rustc-link-lib=static=assimp");
+    println!("cargo:rustc-link-lib=assimp");
     println!("cargo:rustc-link-lib=stdc++");
-
 
     println!("cargo:rerun-if-changed=build.rs");
 }
