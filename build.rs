16b5f8dbdcbb6df98d21706001924f130c65a5e2
Fredrik Noren <fredrik.jw.noren@gmail.com>
2016-04-15 22:47:45 -0400

    Just use pkg-config in build

--- build.rs
+++ build.rs
@@ -1,14 +1,7 @@
-use std::env;
+extern crate pkg_config;
 
 fn main() {
-    let module_path = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let target = env::var("TARGET").unwrap();
-
-    let assimp_path = format!("{}/../pixelport_deps/assimp/v3.2/{}/", module_path, target);
-    println!("cargo:rustc-link-search=native={}", assimp_path);
-
-    println!("cargo:rustc-link-lib=assimp");
-    println!("cargo:rustc-link-lib=stdc++");
-
-    println!("cargo:rerun-if-changed=build.rs");
+    if let Ok(..) = pkg_config::Config::new().atleast_version("3.2.0").find("assimp") {
+        return
+    }
 }
